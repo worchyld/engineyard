031705ddf94be96401b58254606ebc7142df2501
AD <silentflute.ad@gmail.com>
2024-07-02 13:34:00 +0100

    Add sounds enum

---
+++ EngineYard/EngineYard/Game/Sounds/Sounds.swift
@@ -0,0 +1,19 @@
+//
+//  Sounds.swift
+//  EngineYard
+//
+//  Created by Amarjit on 02/07/2024.
+//
+
+import Foundation
+
+enum Sounds: String, CaseIterable {
+    case click = "Birdy_Note5.wav"
+    case notify = "Birdy_Notification1.wav"
+    case cardOpen = "cardSlide8.wav"
+    case cardSlide1 = "cardSlide1.wav"
+    case cardSlide2 = "cardSlide2.wav"
+    case cardSlide3 = "cardSlide3.wav"
+    case dieThrow1 = "dieThrow1.ogg"
+    case dieThrow2 = "dieThrow2.ogg"
+}
